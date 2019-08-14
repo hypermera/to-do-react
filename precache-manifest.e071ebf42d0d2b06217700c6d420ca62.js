c0c59369cfcabd325dc3a6a8111ad07ba132c580
hypermera <hypermera@gmail.com>
2019-08-14 13:56:46 +0300

    Updates

---
+++ precache-manifest.e071ebf42d0d2b06217700c6d420ca62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590b17c99acd975a8abd72834f9cbfa3",
+    "url": "/to-do-react/index.html"
+  },
+  {
+    "revision": "3c89054a1aa7141dace3",
+    "url": "/to-do-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "25670fc2731698e47b45",
+    "url": "/to-do-react/static/js/2.27be089b.chunk.js"
+  },
+  {
+    "revision": "3c89054a1aa7141dace3",
+    "url": "/to-do-react/static/js/main.36693ce9.chunk.js"
+  },
+  {
+    "revision": "b76429878ed8b1f4d799",
+    "url": "/to-do-react/static/js/runtime~main.00a04062.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
